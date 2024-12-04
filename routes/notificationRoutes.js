e987bf4853512fcd83c6142019244f8e2a79607e
jhonticonachambi <67062330+jhonticonachambi@users.noreply.github.com>
2024-12-04 12:12:05 -0500

    version 30

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,19 +1,3 @@
-// // routes/notificationRoutes.js
-// const express = require('express');
-// const router = express.Router();
-// const notificationController = require('../controllers/notificationController');
-// const { authMiddleware } = require('../middlewares/authMiddleware'); // Asegúrate de tener un middleware de autenticación
-
-// // Rutas de notificaciones
-// router.get('/', authMiddleware, notificationController.getNotifications); // Obtener notificaciones
-// router.post('/', authMiddleware, notificationController.createNotification); // Crear notificación
-// router.put('/:notificationId/read', authMiddleware, notificationController.markAsRead); // Marcar como leída
-// router.delete('/:notificationId', authMiddleware, notificationController.deleteNotification); // Eliminar notificación
-
-// module.exports = router;
-
-
-// routes/notificationRoutes.js
 // routes/notificationRoutes.js
 const express = require('express');
 const router = express.Router();
--- routes/postulationRoutes.js
+++ routes/postulationRoutes.js
@@ -1,16 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const postulationController = require('../controllers/PostulationController');
-// // Ruta para crear una nueva postulación
-// router.post('/', postulacionController.crearPostulacion);
-// // Ruta para obtener postulaciones por usuario
-// router.get('/usuario/:userId', postulacionController.obtenerPostulacionesPorUsuario);
-// // Ruta para obtener postulaciones por proyecto
-// router.get('/proyecto/:projectId', postulacionController.obtenerPostulacionesPorProyecto);
-// // Ruta para actualizar el estado de múltiples postulaciones
-// router.put('/actualizar-estado', postulacionController.actualizarEstadoPostulaciones);
-// module.exports = router;
-
 //routes/postulationRoutes
 
 const express = require('express');
