8c2bc34dd7d93085b6634a090261b55146ac1148
jhonticonachambi <67062330+jhonticonachambi@users.noreply.github.com>
2025-05-22 00:01:57 -0500

    v36

--- routes/googleAuthRoutes.js
+++ routes/googleAuthRoutes.js
@@ -12,14 +12,29 @@ const generateToken = (user) => {
 router.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 // Ruta de callback después de la autenticación
+// router.get(
+//   '/callback',
+//   passport.authenticate('google', { failureRedirect: 'https://fronted-five.vercel.app/login' }),
+//   (req, res) => {
+//     // Generar token JWT para el usuario autenticado
+//     const token = generateToken(req.user);
+    
+//     // Redirigir al usuario con el token como parámetro de URL
+//     res.redirect(`https://fronted-five.vercel.app/auth-callback?token=${token}&name=${encodeURIComponent(req.user.name)}&role=${req.user.role}&id=${req.user._id}`);
+//   }
+// );
+
 router.get(
   '/callback',
+  (req, res, next) => {
+    console.log('Callback URL llamada:', req.url);
+    console.log('Query params:', req.query);
+    next();
+  },
   passport.authenticate('google', { failureRedirect: 'https://fronted-five.vercel.app/login' }),
   (req, res) => {
-    // Generar token JWT para el usuario autenticado
+    console.log('Usuario autenticado:', req.user);
     const token = generateToken(req.user);
-    
-    // Redirigir al usuario con el token como parámetro de URL
     res.redirect(`https://fronted-five.vercel.app/auth-callback?token=${token}&name=${encodeURIComponent(req.user.name)}&role=${req.user.role}&id=${req.user._id}`);
   }
 );
